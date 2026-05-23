8904b0980d7c11da83c572ac110553866c579170
Mudhita Bajracharya <mudhitabajracharya@gmail.com>
2026-05-23 18:12:05 +0545

    fixed something idk

--- client/src/utils/GeneratePDF.ts
+++ client/src/utils/GeneratePDF.ts
@@ -4,26 +4,24 @@ import type { IResumeData } from "../types/FormTypes";
 
 export const generatePDF = async (data: IResumeData) => {
 
-    const pages = document.querySelectorAll(".resume-page");
+    const container = document.getElementById("pdf-render");
+    if (!container) return;
 
+    const pages = container.querySelectorAll(".resume-page");
     if (!pages.length) return;
-
     const pdf = new jsPDF("p", "mm", "a4");
+    console.log("pages found:", pages.length);
 
     for (let i = 0; i < pages.length; i++) {
-
         const page = pages[i] as HTMLElement;
-
         const image = await toPng(page, {
             cacheBust: true,
             pixelRatio: 3,
             backgroundColor: "#ffffff",
         });
-
         if (i > 0) {
             pdf.addPage();
         }
-
         pdf.addImage(
             image,
             "PNG",
