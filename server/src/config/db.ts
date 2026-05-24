8dcdbe685a530e6f6a79f8cb2887563702d247fc
Mudhita Bajracharya <mudhitabajracharya@gmail.com>
2026-05-24 17:36:56 +0545

    debug db connection

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -5,13 +5,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        console.log("Attempting DB connection...");
+        console.log("URI exists:", !!process.env.MONGO_URI); 
         const conn = await mongoose.connect(
             process.env.MONGO_URI as string
         );
+        console.log("MongoDB Connected:", conn.connection.host);
         console.log("DB Name:", conn.connection.name);
-
     } catch (error) {
-        console.error("DB Connection Failed:", error);
+        console.error("DB Connection Failed:", error); 
         process.exit(1);
     }
 };
